00ecd6825ab426261c39e3bb3575cb43a05eb952
Panam-Shah <57012223+Panam-Shah@users.noreply.github.com>
2022-11-21 18:02:32 +0530

    Update Commands.ps1

--- Commands.ps1
+++ Commands.ps1
@@ -1,5 +1,5 @@
-Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Error" > Error_log.csv
+Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Error" > Error_log.txt
 
-Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Warning" > Warning_log.csv
+Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Warning" > Warning_log.txt
 
-Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Information" > Information_log.csv
+Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Information" > Information_log.txt

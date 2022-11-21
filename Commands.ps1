8b697009ae32f48b073395f7d0321a03f8799e7c
Panam-Shah <57012223+Panam-Shah@users.noreply.github.com>
2022-11-21 17:56:19 +0530

    Update Commands.ps1

--- Commands.ps1
+++ Commands.ps1
@@ -1,5 +1,1 @@
-powershell -ExecutionPolicy Bypass "Get-EventLog -LogName System -After \"21-11-2022 00:00:00\" -AsBaseObject -Newest 100 -EntryType \"Error\" > Error_log.csv"
-
 powershell -ExecutionPolicy Bypass "Get-EventLog -LogName System -After \"21-11-2022 00:00:00\" -AsBaseObject -Newest 100 -EntryType \"Warning\" > Warning_log.csv"
-
-powershell -ExecutionPolicy Bypass "Get-EventLog -LogName System -After \"21-11-2022 00:00:00\" -AsBaseObject -Newest 100 -EntryType \"Information\" > Information_log.csv"

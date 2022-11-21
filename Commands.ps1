e6c22f23cd3d68efa13b7cd24540a806b63e1278
Panam-Shah <57012223+Panam-Shah@users.noreply.github.com>
2022-11-21 18:09:53 +0530

    Update Commands.ps1

--- Commands.ps1
+++ Commands.ps1
@@ -1,5 +1,11 @@
+echo "Creating Error Logs file"
 Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Error" > Error_log.txt
+echo "Created"
 
+echo "Creating Warning Logs file"
 Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Warning" > Warning_log.txt
+echo "Created"
 
+echo "Creating Information Logs file"
 Get-EventLog -LogName System -After "21-11-2022 00:00:00" -AsBaseObject -Newest 100 -EntryType "Information" > Information_log.txt
+echo "Created"
